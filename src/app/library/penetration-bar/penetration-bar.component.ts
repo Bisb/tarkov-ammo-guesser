07683f283fd72e5867e7700829a7d08a67c8e0cf
Guillaume Adam <guillaume@gadam.fr>
2023-12-26 16:05:08 +0100

    fix: penetration above 70 now displayed properly on the bar

--- src/app/library/penetration-bar/penetration-bar.component.ts
+++ src/app/library/penetration-bar/penetration-bar.component.ts
@@ -4,10 +4,12 @@ import { Ammunition } from '../../models/ammunition';
 @Component({
   selector: 'app-penetration-bar[ammunition]',
   templateUrl: './penetration-bar.component.html',
-  styleUrls: ['./penetration-bar.component.scss']
+  styleUrls: ['./penetration-bar.component.scss'],
 })
 export class PenetrationBarComponent {
   @Input('ammunition') ammunition!: Ammunition;
 
   readonly armorClasses = [...Array(6).keys()].map(v => v + 1);
+
+  readonly min = Math.min;
 }
